a582001a361e493fb075f55775ed3f0862dd29bb
unknown <abodyalex1@gmail.com>
2022-10-04 23:18:39 +0300

    Add food table

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :foods
   devise_for :users
-  
   devise_scope  :user do
     get 'users/sign_out' => 'devise/sessions#destroy'
   end
 
   root 'users#index'
   resources :users
+  
 end
