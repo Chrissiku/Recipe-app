e6592ced71c0666743ea929e52eaf8cc810bc881
chrissiku <chrissiku5@gmail.com>
2022-10-04 16:41:02 +0200

    Add `name` validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation])
   end
 end
