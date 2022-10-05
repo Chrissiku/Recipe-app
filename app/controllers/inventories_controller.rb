b4b261e289eb19ab913f1d610064381781b132f0
safar ali <miqra019@gmail.com>
2022-10-05 19:19:28 +0500

    Add controllers

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,6 +1,13 @@
 class InventoriesController < ApplicationController
-    def index; end
 
-    def show; end
+  def index
+    @inventories = Inventory.all
+  end
+
+
+  def show
+    @inventory = Inventory.joins(:user).where(user: { id: params[:user_id] }).find(params[:id])
+  end
+
   end
   
--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,6 +2,8 @@ class Inventory < ApplicationRecord
 
     belongs_to :user
 
+    has_many :inventory_foods
+
     
     def recent_inventory
         inventories.order(create_at: :desc).limit(3)
