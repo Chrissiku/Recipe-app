8c9b36c97c983385e2d85fa80d5b6307384dc2ac
safar ali <miqra019@gmail.com>
2022-10-05 22:53:18 +0500

    Resolve lintes errors

--- test/models/inventory_test.rb
+++ test/models/inventory_test.rb
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-require "test_helper"
-=======
 require 'test_helper'
->>>>>>> 3091ccde578d0e95909038bee73fecb5c4c00b9b
 
 class InventoryTest < ActiveSupport::TestCase
   # test "the truth" do
