07f940a770e9ac2c4526dd1f72c3aa6f6e7e2761
chrissiku <chrissiku5@gmail.com>
2022-10-05 10:00:31 +0200

    Implement user `Ability`

---
+++ app/models/ability.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+
+    user ||= User.new # guest user (not logged in)
+    can :read, :all
+
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :manage, Food
+      can :read, Recipe, public: true
+      can :manage, Recipe, user_id: user.id
+      can :manage, Inventory, user_id: user.id
+    end
+  end
+end
