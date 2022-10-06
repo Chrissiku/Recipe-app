1925c7a9d849a2824a1ccd78e34caab6eecd6378
chrissiku <chrissiku5@gmail.com>
2022-10-06 10:28:09 +0200

    Update recipes

--- app/models/inventory_food.rb
+++ app/models/inventory_food.rb
@@ -1,4 +1,4 @@
 class InventoryFood < ApplicationRecord
-  belongs_to :food, foreign_key: 'foods_id'
+  belongs_to :food, foreign_key: 'food_id'
   belongs_to :inventory, foreign_key: 'inventory_id'
 end
