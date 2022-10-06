15a8a1a8213fb872bc4487f00b419d4985832e28
chrissiku <chrissiku5@gmail.com>
2022-10-06 11:24:18 +0200

    Implement write test for the `Food` model

---
+++ spec/model/food_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+        expect(Food.new(name: 'Apple')).to be_valid
+    end
+
+    it 'is not valid without a name' do
+        expect(Food.new).to_not be_valid
+    end
+  end
+end
+
+
